6dea8f6f8b1373a61f8438384f01db85a2962eea
Artem Mikhailin <m1r0tvorxc.dev@gmail.com>
2025-07-14 13:25:07 +0300

    Fix: minor fixes

--- backend/services/auth_service/domain/models.go
+++ backend/services/auth_service/domain/models.go
@@ -11,8 +11,8 @@ import (
 // Credentials holds login or registration data
 type RegistrCredentials struct {
 	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
 	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type LoginCredentials struct {
