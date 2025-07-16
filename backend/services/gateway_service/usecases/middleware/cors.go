191d4a28963b97d433ed5ee09b14c981b098f488
Artem Mikhailin <m1r0tvorxc.dev@gmail.com>
2025-07-16 09:02:57 +0300

    Change cors

--- backend/services/gateway_service/usecases/middleware/cors.go
+++ backend/services/gateway_service/usecases/middleware/cors.go
@@ -7,7 +7,7 @@ import (
 // CORSMiddleware
 func CORSMiddleware(frontOrigin string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", frontOrigin)
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
 		if c.Request.Method == "OPTIONS" {
